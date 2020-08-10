7c22b5d8b9a5a9eaaeabd0465e682bd3450029d4
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-10 11:10:01 -0300

    SELECT description of members

--- 2 - Scripts/0 - SELECT.sql
+++ 2 - Scripts/0 - SELECT.sql
@@ -2,3 +2,7 @@ SELECT club_member.member_id, first_name, last_name, description, amount, due_da
 FROM club_member, invoice
 WHERE club_member.member_id = invoice.member_id;
 
+SELECT first_name, middle_name, last_name, professional_title, tier.description
+FROM club_member, tier
+WHERE club_member.tier_code = tier.code
+ORDER BY last_name;
