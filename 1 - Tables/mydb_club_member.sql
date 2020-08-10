ee61a16937e4f0a419490ebc17eb2394689b37f4
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-10 10:38:02 -0300

    Table Club Member

--- 1 - Tables/mydb_club_member.sql
+++ 1 - Tables/mydb_club_member.sql
@@ -1,6 +1,4 @@
 -- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
---
--- Host: localhost    Database: mydb
 -- ------------------------------------------------------
 -- Server version	8.0.19
 
