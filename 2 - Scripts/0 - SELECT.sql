a5426a63cd0b0c42c38429aa8c8df673ea4100b4
BulletSentence <leonardo.a@acad.ifma.edu.br>
2020-08-10 11:36:16 -0300

    SHOW ALL ITENS

--- 2 - Scripts/0 - SELECT.sql
+++ 2 - Scripts/0 - SELECT.sql
@@ -2,7 +2,14 @@ SELECT club_member.member_id, first_name, last_name, description, amount, due_da
 FROM club_member, invoice
 WHERE club_member.member_id = invoice.member_id;
 
+/* -------------------------------------------------------------------------------------------------*/
+
 SELECT first_name, middle_name, last_name, professional_title, tier.description
 FROM club_member, tier
 WHERE club_member.tier_code = tier.code
 ORDER BY last_name;
+
+/* -------------------------------------------------------------------------------------------------*/
+
+SELECT * FROM us_state;
+
